4092411c04ab63b2b205b4f3183ae44ae26dfb10
juan-martz <jmart237@asu.edu>
2023-09-02 00:39:23 -0700

    Updated docstring for methods in slot machine class

--- slot_machine.py
+++ slot_machine.py
@@ -16,6 +16,7 @@ TWO_JQ_WIN = 0.1
 TWO_K_WIN = 0.3
 TWO_A_WON = 1
 
+
 # Slot machine symbols.
 # Getting 3 in a row will win you some $
 # 3 'J's will win 50% of amount bet
@@ -44,7 +45,13 @@ class SlotMachine:
         self.symbols = ["J", "J", "J", "J", "Q", "Q", "Q", "K", "K", "A"]
         random.shuffle(self.symbols)
 
-    def number_of_lines(self):
+    def number_of_lines(self) -> int:
+        """
+        This function will only accept an integer from the user, otherwise,
+        it will keep prompting for an input until an integer is provided.
+        Prompts the user for the number of lines they want to bet on (1-3).
+        :return: int: The number of lines to bet on.
+        """
         while True:
             lines_to_bet_on = input("How many lines do you want to bet on (1-3)?\n")
             if lines_to_bet_on.isdigit():
@@ -57,7 +64,13 @@ class SlotMachine:
                 print("Please enter a number.")
         return lines_to_bet_on
 
-    def get_bet(self):
+    def get_bet(self) -> int:
+        """
+        This function will only accept an integer from the user, otherwise,
+        it will keep prompting for an input until an integer is provided.
+        Prompts the user for the amount they want to bet per line.
+        :return: int: The amount to bet.
+        """
         while True:
             bet = input("How much would you like to bet per line?\n $")
             if bet.isdigit():
@@ -70,14 +83,23 @@ class SlotMachine:
                 print("Please enter a valid $ amount.")
         return bet
 
-    def slot_machine_spin(self):
+    def slot_machine_spin(self) -> list:
+        """
+        This function generates a payline of symbols to display to the player.
+        It does so by using the random module to randomly select 3 items from a pre-defined list of symbols.
+        :return: list: The payline of symbols
+        """
         spin = []
         for _ in range(3):
             symbol = random.choice(self.symbols)
             spin.append(symbol)
         return spin
 
-    def deposit(self):
+    def deposit(self) -> int:
+        """
+        This function takes deposits. The amount entered must be an integer.
+        :return: int: The amount deposited.
+        """
         while True:
             player_deposit = input("Enter an amount to deposit:\n $")
             if player_deposit.isdigit():
@@ -87,19 +109,36 @@ class SlotMachine:
                 print("Please enter a valid $ amount.")
         return player_deposit
 
-    def update_balance(self, total_bet):
+    def update_balance(self, total_bet: int) -> int:
+        """
+        Updates the balance. It updated the data.txt file with the new balance
+        :param total_bet: int
+        :return: int: The updated balance
+        """
         with open("data.txt", 'r') as d:
             self.balance = int(d.read()) - total_bet
         with open("data.txt", 'w') as d:
             d.write(str(self.balance))
         return self.balance
 
-    def cash_out(self):
+    def cash_out(self) -> None:
+        """
+        After typing 'cash out' in the game prompt, the program will immediately end.
+        All variables will be reset to their initial values.
+        Balance will be reset to 0
+        :return:
+        """
         print(f"You have cashed out ${self.balance}, your play balance is $0.")
         with open("data.txt", 'w') as d:
             d.write(str(0))
 
-    def check_win(self, spin, total_bet):
+    def check_win(self, spin: list, total_bet: int):
+        """
+        Checks if a winning payline has been generated.
+        :param spin: list: The payline of symbols
+        :param total_bet: int: The amount bet
+        :return: None
+        """
         if spin[0] == "Q" and spin[1] == "Q" and spin[2] == "Q":
             print(f"You have won ${total_bet * Q}")
             money = self.balance + (total_bet * Q)
@@ -142,4 +181,3 @@ class SlotMachine:
                     data = d.write(str(money))
             else:
                 print("No win.")
-
